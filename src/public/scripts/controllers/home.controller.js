8004c9baaaa1618acaed8575d8f4f98637b59e13
Hunter Rancourt <skwid138@gmail.com>
2017-12-02 17:40:46 -0600

    created controller and service

---
+++ src/public/scripts/controllers/home.controller.js
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+
+/*
+This controller is for the home view.
+- displays 
+*/
+myApp.controller('HomeController', function (GithubService, $location) {
+    console.log('in HomeController');
+    const vm = this;
+
+
+
+    /************** $http **************/
+
+
+
+    /************** on page load **************/
+
+
+
+}); // end HomeController
---
+++ src/public/scripts/services/github.service.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+myApp.service('GithubService', function ($http, $location) {
+    console.log('in GithubService');
+    const self = this;
+
+
+
+/************** $http **************/
+
+
+
+}); // end TrailService
