83382420c1763c39332de1696a8db655d0d91bfa
Hunter Rancourt <skwid138@gmail.com>
2017-12-03 16:40:13 -0600

    on page load controller hits service to set github variables

--- src/public/scripts/controllers/home.controller.js
+++ src/public/scripts/controllers/home.controller.js
@@ -8,14 +8,27 @@ myApp.controller('HomeController', function (GithubService, $location) {
     console.log('in HomeController');
     const vm = this;
 
+    // github user data
+    vm.user = GithubService.user;
+    // github repo data
+    vm.repos = GithubService.repos;
 
 
     /************** $http **************/
 
+    vm.setGithubObjects = () => {
+        console.log('in setGithubObjects');
+        // get github user info
+        GithubService.getGithubUser();
+        // get github repo info
+        GithubService.getGithubRepos();
+    }; // end setGithubObjects
 
 
     /************** on page load **************/
 
+    // get github user and repo info
+    vm.setGithubObjects();
 
 
 }); // end HomeController
