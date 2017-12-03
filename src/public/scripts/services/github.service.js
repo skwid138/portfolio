4ad96ac59190fb4b76eb625bf24f951b1e03b928
Hunter Rancourt <skwid138@gmail.com>
2017-12-03 16:35:30 -0600

    added github API calls to service

--- src/public/scripts/services/github.service.js
+++ src/public/scripts/services/github.service.js
@@ -4,10 +4,38 @@ myApp.service('GithubService', function ($http, $location) {
     console.log('in GithubService');
     const self = this;
 
+    // github user data
+    self.user = { 
+        data: {} 
+    }; // end user
+
+    // github repo data
+    self.repos = { 
+        data: [] 
+    }; // end repos
+
 
 
 /************** $http **************/
 
+    // get user data via github API
+    self.getGithubUser = () => {
+        console.log('in getGithubUser');
+        $http.get('/github/user').then((response) => {
+            // set user object values
+            self.user.data = response.data;
+        }); // end GET
+    }; // end getGithubUser
+
+    // get repo data via github API
+    self.getGithubRepos = () => {
+        console.log('in getGithubRepos');
+        $http.get('/github/repos').then((response) => {
+            // set repos object value
+            self.repos.data = response.data;
+        }); // end GET
+    }; // end getGithubRepos
 
 
-}); // end TrailService
+
+}); // end GithubService
